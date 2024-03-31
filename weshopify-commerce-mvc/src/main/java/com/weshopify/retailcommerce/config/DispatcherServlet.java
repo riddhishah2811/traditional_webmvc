45c99eac33cd6843e3da107261052d222ab583f6
RIDDHI SHAH <riddhishah2222@gmail.com>
2024-04-01 01:53:59 +0530

    added

--- weshopify-commerce-mvc/src/main/java/com/weshopify/retailcommerce/config/DispatcherServlet.java
+++ weshopify-commerce-mvc/src/main/java/com/weshopify/retailcommerce/config/DispatcherServlet.java
@@ -1,6 +1,5 @@
 package com.weshopify.retailcommerce.config;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class DispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
--- weshopify-commerce-mvc/src/main/java/com/weshopify/retailcommerce/config/WeshopifyConfig.java
+++ weshopify-commerce-mvc/src/main/java/com/weshopify/retailcommerce/config/WeshopifyConfig.java
@@ -21,3 +21,4 @@ public class WeshopifyConfig extends WebMvcConfigurerAdapter{
 	}
 	
 }
+
